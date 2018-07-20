2e3aa120cac5b76dd9b80befbb0839304dfe9db8
jcampbel <jcampbel@EC9WSSSE89>
2018-07-20 13:42:43 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,120 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASPNetIdentity.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var username = "jimmy@jamesz.com";
+            var password = "Password123!";
+
+            var userStore = new CustomUserStore(new CustomUserDbContext());
+            var userManager = new UserManager<CustomUser, int>(userStore);
+
+            var creationResult = userManager.Create(new CustomUser { UserName = username }, password);
+            Console.WriteLine("Created: {0}", creationResult.Succeeded);
+            Console.ReadLine();
+
+            var user = userManager.FindByName(username);
+
+            //var claimResult = userManager.AddClaim(user.Id, new System.Security.Claims.Claim("given_name", "James"));
+            //Console.WriteLine("Claim: {0}", claimResult.Succeeded);
+
+            var isMatch = userManager.CheckPassword(user, password);
+            Console.WriteLine("Password Match: {0}", isMatch);
+
+
+            Console.ReadLine();
+        }
+    }
+    public class CustomUser : IUser<int>
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+    }
+    public class CustomUserDbContext : DbContext
+    {
+        public CustomUserDbContext() : base("DefaultConnection") { }
+        public DbSet<CustomUser> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            var user = modelBuilder.Entity<CustomUser>();
+            user.ToTable("Users");
+            user.HasKey(x => x.Id);
+            user.Property(x => x.Id).IsRequired().HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+
+            user.Property(x => x.UserName).IsRequired().HasMaxLength(256)
+                .HasColumnAnnotation("Index", new IndexAnnotation(new IndexAttribute("UserNameIndex") { IsUnique = true }));
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+    public class CustomUserStore : IUserPasswordStore<CustomUser, int>
+    {
+        private readonly CustomUserDbContext context;
+        public CustomUserStore(CustomUserDbContext context)
+        {
+            this.context = context;
+        }
+        public Task CreateAsync(CustomUser user)
+        {
+            context.Users.Add(user);
+            return context.SaveChangesAsync();
+        }
+
+        public Task DeleteAsync(CustomUser user)
+        {
+            context.Users.Remove(user);
+            return context.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+
+        public Task<CustomUser> FindByIdAsync(int userId)
+        {
+            return context.Users.FirstOrDefaultAsync(x => x.Id == userId);
+        }
+
+        public Task<CustomUser> FindByNameAsync(string userName)
+        {
+            return context.Users.FirstOrDefaultAsync(x => x.UserName == userName);
+        }
+
+        public Task<string> GetPasswordHashAsync(CustomUser user)
+        {
+            return Task.FromResult(user.PasswordHash);
+        }
+
+        public Task<bool> HasPasswordAsync(CustomUser user)
+        {
+            return Task.FromResult(user.PasswordHash != null);
+        }
+
+        public Task SetPasswordHashAsync(CustomUser user, string passwordHash)
+        {
+            user.PasswordHash = passwordHash;
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateAsync(CustomUser user)
+        {
+            context.Users.Attach(user);
+            return context.SaveChangesAsync();
+        }
+    }
+}
